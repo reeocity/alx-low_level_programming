0f6710f3c093ca872e6bb7cb62936382d7a7c11a
Ibrahim <ibraheemreedwan100@gmail.com>
2023-03-16 18:54:46 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A program that print a line to the standard error
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
